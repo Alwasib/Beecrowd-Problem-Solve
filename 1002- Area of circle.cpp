1c65831d6b1973381e298fd74655873ab680365d
Al- Wasib <122206303+Alwasib@users.noreply.github.com>
2024-03-23 19:12:43 +0600

    Add files via upload

---
+++ 1002- Area of circle.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    double A,R,n=3.14159;
+    cin>>R;
+    A=n*R*R;
+    cout<<fixed<<setprecision(4);
+    cout<<"A="<<A<<endl;
+
+
+    return 0;
+}
