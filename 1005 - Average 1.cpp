b35d9f80e9792403fd1f7cc0cda5dffdc276376a
Al- Wasib <122206303+Alwasib@users.noreply.github.com>
2024-03-23 19:14:30 +0600

    Add files via upload

---
+++ 1005 - Average 1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double A=3.5,B=7.5,MEDIA;
+    float a,b;
+    cin>>a>>b;
+    MEDIA=(a*A+b*B)/(A+B);
+    cout<<fixed<<setprecision(5);
+    cout<<"MEDIA = "<<MEDIA<<endl;
+    return 0;
+}
+
