daeedb7c22ce3f2575286ecfed7445e76b2d1c05
Al- Wasib <122206303+Alwasib@users.noreply.github.com>
2024-03-24 20:05:42 +0600

    Add files via upload

---
+++ 1008 - Salary.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int a,b;
+  float c,d;
+  cin>>a>>b>>c;
+  cout<<"NUMBER = "<<a<<endl;
+  d=b*c;
+  cout<<fixed<<setprecision(2);
+  cout<<"SALARY = U$ "<<d<<endl;
+  return 0;
+}
+
