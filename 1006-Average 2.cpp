7dca41938e1ad4df410893f5fc4052cfe98a55e6
Al- Wasib <122206303+Alwasib@users.noreply.github.com>
2024-03-24 20:04:53 +0600

    Add files via upload

---
+++ 1006-Average 2.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float A,B,C,MEDIA;
+    cin>>A>>B>>C;
+    MEDIA=(((A*2)+(B*3)+(C*5))/10);
+    cout<<fixed<<setprecision(1);
+    cout<<"MEDIA = "<<MEDIA<<endl;
+    return 0;
+}
