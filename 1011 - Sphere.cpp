50032346386c2e9b1b95f80b790669060927b66a
alwasib <122206303+Alwasib@users.noreply.github.com>
2024-03-25 21:57:23 +0600

    Add files via upload

---
+++ 1011 - Sphere.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double pie=3.14159,R,p;
+    cin>>R;
+    p=(4.0/3)*pie*R*R*R;
+    cout<<fixed<<setprecision(3);
+    cout<<"VOLUME = "<<p<<endl;
+    return 0;
+}
