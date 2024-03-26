8ef92c01745145c0459ee40789842bae4eab0f49
alwasib <122206303+Alwasib@users.noreply.github.com>
2024-03-26 20:05:15 +0600

    Add files via upload

---
+++ 1014-Consumption.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int X;
+    float Y,avg;
+    cin>>X>>Y;
+    avg=X/Y;
+    cout<<fixed<<setprecision(3);
+    cout<<avg<<" km/l"<<endl;
+    return 0;
+
+}
+
