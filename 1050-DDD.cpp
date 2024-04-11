b861f74398881f2e4221d713b45314276b392ee1
alwasib <122206303+Alwasib@users.noreply.github.com>
2024-04-11 19:57:49 +0600

    Add files via upload

---
+++ 1050-DDD.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    if(x==61)
+    {
+        cout<<"Brasilia"<<endl;
+    }
+    else if(x==71)
+    {
+        cout<<"Salvador"<<endl;
+    }
+    else if(x==11)
+    {
+        cout<<"Sao Paulo"<<endl;
+    }
+    else if(x==21)
+    {
+        cout<<"Rio de Janeiro"<<endl;
+    }
+    else if(x==32)
+    {
+        cout<<"Juiz de Fora"<<endl;
+    }
+    else if(x==19)
+    {
+        cout<<"Campinas"<<endl;
+    }
+    else if(x==27)
+    {
+        cout<<"Vitoria"<<endl;
+    }
+    else if(x==31)
+    {
+        cout<<"Belo Horizonte"<<endl;
+    }
+    else
+    {
+        cout<<"DDD nao cadastrado"<<endl;
+    }
+}
+
