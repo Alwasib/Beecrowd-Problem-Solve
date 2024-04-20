8f228bb4a1eff5fab870233e03a5fb49867d22b1
alwasib <122206303+Alwasib@users.noreply.github.com>
2024-04-20 20:09:08 +0600

    Add files via upload

---
+++ 1070-Six odd numbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int i,x;
+  cin>>x;
+  for(i=x;i<=x+11;i++)
+  {
+      if(i%2==1||i%2==-1)
+      {
+          cout<<i<<endl;
+      }
+  }
+}
