18714f19259d6c069866917ee35de74c1b222b74
alwasib <122206303+Alwasib@users.noreply.github.com>
2024-04-28 23:13:00 +0600

    Add files via upload

---
+++ 1073-Even Square.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int x,i;
+   cin>>x;
+   for(i=1;i<=x;i++)
+   {
+       if(i%2==0)
+       {
+           cout<<i<<"^2 = "<<i*i<<endl;
+       }
+   }
+}
