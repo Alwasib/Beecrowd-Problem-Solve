78b7bb5352718c54e7d2e196893566b26e17f753
alwasib <122206303+Alwasib@users.noreply.github.com>
2024-05-12 21:37:17 +0600

    Add files via upload

---
+++ 1097-Sequence IJ 3.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j=7;
+    for(i=1;i<=3;i++)
+    {
+      cout<<"I=1 J="<<j<<endl;
+      j--;
+    }
+    j=9;
+     for(i=1;i<=3;i++)
+    {
+      cout<<"I=3 J="<<j<<endl;
+      j--;
+    }
+    j=11;
+     for(i=1;i<=3;i++)
+    {
+      cout<<"I=5 J="<<j<<endl;
+      j--;
+    }
+    j=13;
+    for(i=1;i<=3;i++)
+    {
+      cout<<"I=7 J="<<j<<endl;
+      j--;
+    }
+     j=15;
+    for(i=1;i<=3;i++)
+    {
+      cout<<"I=9 J="<<j<<endl;
+      j--;
+    }
+
+}
+
+
+
