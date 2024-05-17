d024e668880d0a44fa182904aecdc3fb4db49ebf
alwasib <122206303+Alwasib@users.noreply.github.com>
2024-05-17 20:14:35 +0600

    Add files via upload

---
+++ 1098-Sequence IJ4.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    float j=1;
+    for(i=1;i<=3;i++)
+    {
+      cout<<"I=0 J="<<j<<endl;
+      j++;
+    }
+    j=1.2;
+     for(i=1;i<=3;i++)
+    {
+      cout<<"I=0.2 J="<<j<<endl;
+      j++;
+    }
+    j=1.4;
+     for(i=1;i<=3;i++)
+    {
+      cout<<"I=0.4 J="<<j<<endl;
+      j++;
+    }
+    j=1.6;
+    for(i=1;i<=3;i++)
+    {
+      cout<<"I=0.6 J="<<j<<endl;
+      j++;
+    }
+     j=1.8;
+    for(i=1;i<=3;i++)
+    {
+      cout<<"I=0.8 J="<<j<<endl;
+      j++;
+    }
+    j=2;
+    for(i=1;i<=3;i++)
+    {
+      cout<<"I=1 J="<<j<<endl;
+      j++;
+    }
+     j=2.2;
+    for(i=1;i<=3;i++)
+    {
+      cout<<"I=1.2 J="<<j<<endl;
+      j++;
+    }
+     j=2.4;
+    for(i=1;i<=3;i++)
+    {
+      cout<<"I=1.4 J="<<j<<endl;
+      j++;
+    }
+     j=2.6;
+    for(i=1;i<=3;i++)
+    {
+      cout<<"I=1.6 J="<<j<<endl;
+      j++;
+    }
+     j=2.8;
+    for(i=1;i<=3;i++)
+    {
+      cout<<"I=1.8 J="<<j<<endl;
+      j++;
+    }
+     j=3;
+    for(i=1;i<=3;i++)
+    {
+      cout<<"I=2 J="<<j<<endl;
+      j++;
+    }
+
+}
+
+
+
